7f49c4f7e4a25e810400929e240e2cb6d6ce3a37
Amaan56 <shaikhamaan257@gmail.com>
2020-11-12 05:11:17 +0530

    Header

---
+++ src/Header.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import './Header.css';
+import MenuIcon from '@material-ui/icons/Menu';
+import SearchIcon from '@material-ui/icons/Search';
+import VideoCallIcon from '@material-ui/icons/VideoCall';
+import AppsIcon from '@material-ui/icons/Apps';
+import NotificationsIcon from '@material-ui/icons/Notifications';
+import Avatar from '@material-ui/core/Avatar';
+
+function Header() {
+  return (
+    <div className="header">
+      <div className="header__left">
+        <MenuIcon style={{ color: '#5e5d5d' }} />
+        <img
+          className="header__leftLogo"
+          src="https://www.transparentpng.com/thumb/youtube-logo/hd-youtube-logo-image-5.png"
+          alt="Youtube logo"
+        />
+      </div>
+      <div className="header__input">
+        <input type="text" placeholder="Search" />
+        <SearchIcon className="header__inputSearch" />
+      </div>
+      <div className="header__icons">
+        <VideoCallIcon className="header__icon" />
+        <AppsIcon className="header__icon" />
+        <NotificationsIcon className="header__icon" />
+        <Avatar style={{ backgroundColor: 'orange' }}>A</Avatar>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
