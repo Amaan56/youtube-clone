cd3e820aab890706f3391c0c2d179dfc4c4a0add
Amaan56 <shaikhamaan257@gmail.com>
2020-11-13 22:05:46 +0530

    Scrolling functioanality added

--- src/ScrollingMenu.js
+++ src/ScrollingMenu.js
@@ -30,16 +30,44 @@ function ScrollingMenu() {
     'Preofessional Wrestling',
     'React',
   ]);
+
+  const [style, setStyle] = useState({});
+  const [counter, setCounter] = useState(0);
+
+  const scrollLeft = () => {
+    setCounter((prevState, props) => prevState + 150);
+    console.log(counter);
+    console.log('Left');
+    setStyle({
+      transform: `translate(${counter}px)`,
+    });
+  };
+
+  const scrollRight = () => {
+    setCounter((prevState, props) => prevState - 150);
+    console.log(counter);
+    console.log('Right');
+    setStyle({
+      transform: `translate(${counter}px)`,
+    });
+  };
+
   return (
     <div className="scrollingmenu">
+      <div className="scrollingmenu__left" onClick={scrollLeft}>
+        &lt;
+      </div>
       <div className="scrollingmenu__container">
-        {tags.map((tag) => {
+        {tags.map((tag, i) => {
           return (
-            <div className="scrollingmenu__tag">
+            <div className="scrollingmenu__tag" key={i} style={style}>
               <p>{tag}</p>
             </div>
           );
         })}
+      </div>
+      <div className="scrollingmenu__right" onClick={scrollRight}>
+        &gt;
       </div>
     </div>
   );
