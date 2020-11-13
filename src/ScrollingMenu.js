2244eeac63d0ac622e45ef6267e4926819fae2f4
Amaan56 <shaikhamaan257@gmail.com>
2020-11-14 02:52:00 +0530

    Scrolling menu with functionality

--- src/ScrollingMenu.js
+++ src/ScrollingMenu.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './ScrollingMenu.css';
 
 function ScrollingMenu() {
@@ -9,66 +9,127 @@ function ScrollingMenu() {
     'React',
     'Jimmy Kimmel',
     'Tax',
-    'React',
-    'Stocks',
-    'Tax',
-    'Preofessional Wrestling',
-    'React',
-    'Jimmy Kimmel',
-    'Preofessional Wrestling',
-    'React',
-    'Jimmy Kimmel',
-    'Preofessional Wrestling',
-    'React',
-    'Jimmy Kimmel',
-    'Preofessional Wrestling',
-    'React',
-    'Jimmy Kimmel',
-    'Preofessional Wrestling',
-    'React',
-    'Jimmy Kimmel',
-    'Preofessional Wrestling',
-    'React',
+    'Rufus',
+    'Bear',
+    'Dakota',
+    'Fido',
+    'Vanya',
+    'Samuel',
+    'Koani',
+    'Volodya',
+    'Prince',
+    'Yiska',
+    'Star Wars',
+    'This is us',
+    'How I met your mother',
   ]);
 
-  const [style, setStyle] = useState({});
   const [counter, setCounter] = useState(0);
+  const [leftNavigationShow, setLeftNavigationShow] = useState(false);
+  const [rightNavigationShow, setRightNavigationShow] = useState(true);
+  const [firstElementStartLeft, setfirstElementStartLeft] = useState(0);
+
+  const scrollContainer = React.createRef();
+
+  let scrollElements = [];
+
+  useEffect(() => {
+    //console.log(scrollElements[0].getBoundingClientRect().left);
+    setfirstElementStartLeft(scrollElements[0].getBoundingClientRect().left);
+  }, []);
+
+  useEffect(() => {
+    console.log('Counter changed');
+
+    const scrollContainerLeft = scrollContainer.current.getBoundingClientRect()
+      .left;
+    const scrollContainerRight = scrollContainer.current.getBoundingClientRect()
+      .right;
+    const scrollFirstElementLeft = scrollElements[0].getBoundingClientRect()
+      .left;
+    const scrollLastElementRight = scrollElements[
+      scrollElements.length - 1
+    ].getBoundingClientRect().right;
+
+    if (scrollLastElementRight < scrollContainerRight) {
+      setRightNavigationShow(false);
+    }
+
+    if (scrollFirstElementLeft > firstElementStartLeft) {
+      setLeftNavigationShow(false);
+    }
+  }, [counter]);
 
   const scrollLeft = () => {
-    setCounter((prevState, props) => prevState + 150);
-    console.log(counter);
+    const scrollContainerLeft = scrollContainer.current.getBoundingClientRect()
+      .left;
+    const scrollContainerRight = scrollContainer.current.getBoundingClientRect()
+      .right;
+    const scrollFirstElementLeft = scrollElements[0].getBoundingClientRect()
+      .left;
+    const scrollLastElementRight = scrollElements[
+      scrollElements.length - 1
+    ].getBoundingClientRect().right;
+
+    if (scrollFirstElementLeft > firstElementStartLeft) {
+      setLeftNavigationShow(false);
+    } else {
+      setCounter((prevState, props) => prevState + 100);
+      setRightNavigationShow(true);
+    }
     console.log('Left');
-    setStyle({
-      transform: `translate(${counter}px)`,
-    });
   };
 
   const scrollRight = () => {
-    setCounter((prevState, props) => prevState - 150);
-    console.log(counter);
+    const scrollContainerLeft = scrollContainer.current.getBoundingClientRect()
+      .left;
+    const scrollContainerRight = scrollContainer.current.getBoundingClientRect()
+      .right;
+    const scrollFirstElementLeft = scrollElements[0].getBoundingClientRect()
+      .left;
+    const scrollLastElementRight = scrollElements[
+      scrollElements.length - 1
+    ].getBoundingClientRect().right;
+
+    setLeftNavigationShow(true);
+
+    if (scrollLastElementRight > scrollContainerRight) {
+      setCounter((prevState, props) => prevState - 100);
+    } else {
+      setRightNavigationShow(false);
+    }
+
     console.log('Right');
-    setStyle({
-      transform: `translate(${counter}px)`,
-    });
   };
 
   return (
     <div className="scrollingmenu">
-      <div className="scrollingmenu__left" onClick={scrollLeft}>
-        &lt;
-      </div>
-      <div className="scrollingmenu__container">
+      {leftNavigationShow && (
+        <div className="scrollingmenu__left" onClick={scrollLeft}>
+          &lt;
+        </div>
+      )}
+      <div ref={scrollContainer} className="scrollingmenu__container">
         {tags.map((tag, i) => {
           return (
-            <div className="scrollingmenu__tag" key={i} style={style}>
+            <div
+              ref={(ref) => {
+                scrollElements[i] = ref;
+              }}
+              className="scrollingmenu__tag"
+              key={i}
+              style={{ transform: `translate(${counter}px)` }}
+            >
               <p>{tag}</p>
             </div>
           );
         })}
       </div>
-      <div className="scrollingmenu__right" onClick={scrollRight}>
-        &gt;
-      </div>
+      {rightNavigationShow && (
+        <div className="scrollingmenu__right" onClick={scrollRight}>
+          &gt;
+        </div>
+      )}
     </div>
   );
 }
