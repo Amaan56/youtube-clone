e98fddb530d4001e214dd97276abfb5a1d2a730e
Amaan56 <shaikhamaan257@gmail.com>
2020-11-14 15:45:52 +0530

    Multiple videos

--- src/RecommendedSection.js
+++ src/RecommendedSection.js
@@ -15,6 +15,106 @@ function RecommendedSection() {
       views: '114K',
       uploadDate: '1 month ago',
     },
+    {
+      thumbnail:
+        'https://i.ytimg.com/an_webp/ltEpCT8ydiE/mqdefault_6s.webp?du=3000&sqp=CJPZvf0F&rs=AOn4CLC1ncx4Mr9XmqhcxcSl3MjnEvcUXQ',
+      avatar:
+        'https://yt3.ggpht.com/a-/AOh14Gjrkz-bV9ll4OR_m9M3xXEINuwhbBq30F4asg=s68-c-k-c0x00ffffff-no-rj-mo',
+      title: 'How Paul Heyman fixed Roman Reigns',
+      creator: 'Whatculture Wrestling',
+      views: '114K',
+      uploadDate: '1 month ago',
+    },
+    {
+      thumbnail:
+        'https://i.ytimg.com/an_webp/ltEpCT8ydiE/mqdefault_6s.webp?du=3000&sqp=CJPZvf0F&rs=AOn4CLC1ncx4Mr9XmqhcxcSl3MjnEvcUXQ',
+      avatar:
+        'https://yt3.ggpht.com/a-/AOh14Gjrkz-bV9ll4OR_m9M3xXEINuwhbBq30F4asg=s68-c-k-c0x00ffffff-no-rj-mo',
+      title: 'How Paul Heyman fixed Roman Reigns',
+      creator: 'Whatculture Wrestling',
+      views: '114K',
+      uploadDate: '1 month ago',
+    },
+    {
+      thumbnail:
+        'https://i.ytimg.com/an_webp/ltEpCT8ydiE/mqdefault_6s.webp?du=3000&sqp=CJPZvf0F&rs=AOn4CLC1ncx4Mr9XmqhcxcSl3MjnEvcUXQ',
+      avatar:
+        'https://yt3.ggpht.com/a-/AOh14Gjrkz-bV9ll4OR_m9M3xXEINuwhbBq30F4asg=s68-c-k-c0x00ffffff-no-rj-mo',
+      title: 'How Paul Heyman fixed Roman Reigns',
+      creator: 'Whatculture Wrestling',
+      views: '114K',
+      uploadDate: '1 month ago',
+    },
+    {
+      thumbnail:
+        'https://i.ytimg.com/an_webp/ltEpCT8ydiE/mqdefault_6s.webp?du=3000&sqp=CJPZvf0F&rs=AOn4CLC1ncx4Mr9XmqhcxcSl3MjnEvcUXQ',
+      avatar:
+        'https://yt3.ggpht.com/a-/AOh14Gjrkz-bV9ll4OR_m9M3xXEINuwhbBq30F4asg=s68-c-k-c0x00ffffff-no-rj-mo',
+      title: 'How Paul Heyman fixed Roman Reigns',
+      creator: 'Whatculture Wrestling',
+      views: '114K',
+      uploadDate: '1 month ago',
+    },
+    {
+      thumbnail:
+        'https://i.ytimg.com/an_webp/ltEpCT8ydiE/mqdefault_6s.webp?du=3000&sqp=CJPZvf0F&rs=AOn4CLC1ncx4Mr9XmqhcxcSl3MjnEvcUXQ',
+      avatar:
+        'https://yt3.ggpht.com/a-/AOh14Gjrkz-bV9ll4OR_m9M3xXEINuwhbBq30F4asg=s68-c-k-c0x00ffffff-no-rj-mo',
+      title: 'How Paul Heyman fixed Roman Reigns',
+      creator: 'Whatculture Wrestling',
+      views: '114K',
+      uploadDate: '1 month ago',
+    },
+    {
+      thumbnail:
+        'https://i.ytimg.com/an_webp/ltEpCT8ydiE/mqdefault_6s.webp?du=3000&sqp=CJPZvf0F&rs=AOn4CLC1ncx4Mr9XmqhcxcSl3MjnEvcUXQ',
+      avatar:
+        'https://yt3.ggpht.com/a-/AOh14Gjrkz-bV9ll4OR_m9M3xXEINuwhbBq30F4asg=s68-c-k-c0x00ffffff-no-rj-mo',
+      title: 'How Paul Heyman fixed Roman Reigns',
+      creator: 'Whatculture Wrestling',
+      views: '114K',
+      uploadDate: '1 month ago',
+    },
+    {
+      thumbnail:
+        'https://i.ytimg.com/an_webp/ltEpCT8ydiE/mqdefault_6s.webp?du=3000&sqp=CJPZvf0F&rs=AOn4CLC1ncx4Mr9XmqhcxcSl3MjnEvcUXQ',
+      avatar:
+        'https://yt3.ggpht.com/a-/AOh14Gjrkz-bV9ll4OR_m9M3xXEINuwhbBq30F4asg=s68-c-k-c0x00ffffff-no-rj-mo',
+      title: 'How Paul Heyman fixed Roman Reigns',
+      creator: 'Whatculture Wrestling',
+      views: '114K',
+      uploadDate: '1 month ago',
+    },
+    {
+      thumbnail:
+        'https://i.ytimg.com/an_webp/ltEpCT8ydiE/mqdefault_6s.webp?du=3000&sqp=CJPZvf0F&rs=AOn4CLC1ncx4Mr9XmqhcxcSl3MjnEvcUXQ',
+      avatar:
+        'https://yt3.ggpht.com/a-/AOh14Gjrkz-bV9ll4OR_m9M3xXEINuwhbBq30F4asg=s68-c-k-c0x00ffffff-no-rj-mo',
+      title: 'How Paul Heyman fixed Roman Reigns',
+      creator: 'Whatculture Wrestling',
+      views: '114K',
+      uploadDate: '1 month ago',
+    },
+    {
+      thumbnail:
+        'https://i.ytimg.com/an_webp/ltEpCT8ydiE/mqdefault_6s.webp?du=3000&sqp=CJPZvf0F&rs=AOn4CLC1ncx4Mr9XmqhcxcSl3MjnEvcUXQ',
+      avatar:
+        'https://yt3.ggpht.com/a-/AOh14Gjrkz-bV9ll4OR_m9M3xXEINuwhbBq30F4asg=s68-c-k-c0x00ffffff-no-rj-mo',
+      title: 'How Paul Heyman fixed Roman Reigns',
+      creator: 'Whatculture Wrestling',
+      views: '114K',
+      uploadDate: '1 month ago',
+    },
+    {
+      thumbnail:
+        'https://i.ytimg.com/an_webp/ltEpCT8ydiE/mqdefault_6s.webp?du=3000&sqp=CJPZvf0F&rs=AOn4CLC1ncx4Mr9XmqhcxcSl3MjnEvcUXQ',
+      avatar:
+        'https://yt3.ggpht.com/a-/AOh14Gjrkz-bV9ll4OR_m9M3xXEINuwhbBq30F4asg=s68-c-k-c0x00ffffff-no-rj-mo',
+      title: 'How Paul Heyman fixed Roman Reigns',
+      creator: 'Whatculture Wrestling',
+      views: '114K',
+      uploadDate: '1 month ago',
+    },
   ];
 
   return (
